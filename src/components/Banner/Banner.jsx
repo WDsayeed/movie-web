e4dde70f2140d592330f454bd3ff7526bd0471eb
WDsayeed <sayeedahammed526@gmail.comm>
2023-06-02 17:34:19 +0600

    added table

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,19 +2,23 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import './Banner.css'
 
+import img1 from '../../assets/image/banner1.jpg'
+import img2 from '../../assets/image/banner2.jpg'
+import img3 from '../../assets/image/banner3.jpg'
+
 const Banner = () => {
   return (
     <div className="text-center">
         
       <Carousel>
         <div className="banner">
-          <img src="../../../public/image/banner1.jpg" />
+          <img src={img1} />
         </div>
         <div className="banner">
-          <img src="../../../public/image/banner2.jpg" />
+          <img src={img2} />
         </div>
         <div className="banner">
-          <img src="../../../public/image/banner3.jpg" />
+          <img src={img3} />
         </div>
       </Carousel>
     </div>
